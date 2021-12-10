d090b18d3d33970567680f56e52cecc50ecc8ba1
Peter A <ink.splatters@pm.me>
2021-12-10 12:41:43 +0100

    bootstrap fixes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # go-redis-k8s-poc
 ACID compliant REST CRUD using Redis for persistence, deployed in k8s
+
+## Getting started
+
+there is bootstrap script at the root of the repo which installs tooling and sets up the cluster
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
 
+set -e
 ####################################################################################################
 
 #l=(go kind k9s kubectl redis) # redis included just to test rapidly, without k8s
 tools=(go kind k9s kubectl)
+
+cmd=(install)
+pkg_mgr=brew
 
 ####################################################################################################
 
 if [[ $(uname) != "Darwin" ]]; then
 	# shellcheck disable=SC2046
 	# shellcheck disable=SC2086
-	echo For $(uname) please manually install the following tools: ${tools[*]}
-	exit 1
+	echo WARN: $(uname) is not supported for installing the tools. You are on your own.
+	echo "      the tools expected: ${tools[*]}"
+	echo "      cluster bootstrap still probably would work"
+	tools=()
+	pkg_mgr=
+	cmd=
 fi
 
 ####################################################################################################
 
-function cecho(){
-  # shellcheck disable=SC2068
-  echo $@
+function cecho() {
+	# shellcheck disable=SC2068
+	echo $@
 }
 
-set -e
-cmd=(install)
-pkg_mgr=brew
-
 function manually() {
-  echo ERROR: $1 is missing. Please install it manually.
+	echo ERROR: $1 is missing. Please install it manually.
 }
 function prep() {
 	if ! command -v docker >/dev/null 2>&1; then
-      manually docker
-      exit 1
-  fi
-
+		manually docker
+		exit 1
+	fi
 
 	for arg in "$@"; do
 		if [[ $arg =~ reinstall ]]; then
@@ -54,15 +57,15 @@ function info() {
 }
 
 function run() {
-  read -e -p "-- $* [press 'n' to skip]" -n 1 -r
-  REPLY=${REPLY:-Y}
-  if [[  $REPLY =~ ^[Yy]$ ]]; then
-  	set -x
-  	# shellcheck disable=SC2048
-  	$*
-  	set +x
-  fi
-  echo
+	read -e -p "-- $* [press 'n' to skip]" -n 1 -r
+	REPLY=${REPLY:-Y}
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		set -x
+		# shellcheck disable=SC2048
+		$*
+		set +x
+	fi
+	echo
 }
 
 ####################################################################################################
@@ -75,8 +78,10 @@ info
 # shellcheck disable=SC2154
 # shellcheck disable=SC2128
 
-####################################################################################################
-run $pkg_mgr ${cmd[*]} ${tools[*]}
+if [[ $(uname) == "Darwin" ]]; then
+  run $pkg_mgr ${cmd[*]} ${tools[*]}
+fi
+
 
 ####################################################################################################
 script_dir=$(dirname "${BASH_SOURCE[0]}")
@@ -84,5 +89,3 @@ cmd_create_cluster=(kind create cluster "--config=$script_dir/k8s/kind.yaml")
 activate_cluster=(kubectl cluster-info --context kind-kind)
 run ${cmd_create_cluster[*]}
 run ${activate_cluster[*]}
-
-
