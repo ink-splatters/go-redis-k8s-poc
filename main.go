c01843c3083d0a09f18dc129c200c0009163645a
Peter A <ink.splatters@pm.me>
2021-12-12 14:35:42 +0100

    scratch

--- main.go
+++ main.go
@@ -1,16 +1,6 @@
 package main
 
-import (
-	_ "context"
-	_ "github.com/go-redis/redis/v8"
-)
-
 func main() {
 
-	//ctx := context.Background()
-
-	//rdb := redis.NewClient(&redis.Options{
-	//	Addr: ":6379",
-	//})
 
 }
