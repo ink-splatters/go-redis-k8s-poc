d584e6558bbb056d993b9e0d2aa46783e159a5a1
Peter A <ink.splatters@pm.me>
2021-12-13 02:24:54 +0100

    vanish

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	_ "context"
-	"fmt"
-	"github.com/SuperPaintman/nice/cli"
 	_ "github.com/go-redis/redis/v8"
 )
 
@@ -19,30 +17,4 @@ var (
 )
 
 func main() {
-
-	app := cli.App{
-		Name:  AppName,
-		Usage: cli.Usage("PoC of ACID compliant REST CRUD (k8s, reddis for persistence)"),
-
-
-		Action: cli.ActionFunc(func(cmd *cli.Command) cli.ActionRunner {
-			name := cli.StringArg(cmd, "name",
-				cli.Usage("Who we say hello to"),
-				cli.Optional,
-			)
-			*name = "Nice" // Default value.
-
-			return func(cmd *cli.Command) error {
-				fmt.Sprintf("%s (build %s)", Version, Build)
-				return nil
-			}
-		}),
-		CommandFlags: []cli.CommandFlag{
-			cli.HelpCommandFlag(),
-			cli.VersionCommandFlag(fmt.Sprintf("%s (build %s)", Version, Build)),
-		},
-	}
-
-	app.HandleError(app.Run())
-
 }
