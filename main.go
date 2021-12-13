86bffebf4ec0a00877c209211e03f924ed99ad02
Peter A <ink.splatters@pm.me>
2021-12-13 11:27:52 +0100

    archived

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	_ "context"
-	_ "github.com/go-redis/redis/v8"
+	"github.com/go-redis/redis/v8"
+	_ "github.com/davecgh/go-spew/spew"
 )
+type Address struct {
+	Street string `redis:"street"`
+	House int `redis:"house"`
+}
+
+type Phone struct {
+	Type string  `redis:"type"`
+	Number string `redis:"number"`
+}
+
+type Contact struct {
+	Address `redis:"address"`
+	Phone	`redis:"phone"`
+}
+
+type  Person struct {
+	Id int64 `redis:"name"`
+	Name string `redis:"name"`
+	Surname string `redis:"surname"`
+	Age int8 `redis:"age"`
+	Primary Contact `redis:"primary"`
+	Secondary []Contact `redis:"secondary"`
+}
+
 
 var (
 	// Version (set via LDFLAGS)
@@ -17,4 +44,26 @@ var (
 )
 
 func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: ":6379",
+	})
+
+	p := Person{
+		Id: 1,
+		Name: "Peter",
+		Surname: "A",
+		Age: 1,
+		Primary: Contact{
+			Address{
+				Street: "street",
+				House: 1,
+			},
+			Phone{
+				Type: "landline",
+				Number: "123",
+			},
+		},
+	}
+
+	rdb.H
 }
